bb37387dd55707e1d1b3da3b02c6c5f73a788d3d
jvftw <jvmbworks@gmail.com>
2023-04-11 23:18:53 -0400

    Aula Arrays e Repetição completa

---
+++ aulaArrays.js
@@ -0,0 +1,17 @@
+const alunos = ["João", "Vítor", "José"];
+
+alunos.push('Renan');
+console.log(alunos);
+
+const notas = [];
+
+notas.push(7);
+notas.push(8);
+notas.push(10);
+console.log(notas);
+
+const soma = notas[0] + notas[1] + notas [2];
+
+for (let i = 0; i < 100; i++) { // setup do iterable; boolean; incrementa 1 a i
+    console.log(i)
+}
